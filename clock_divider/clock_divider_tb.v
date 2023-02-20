a46fbdbbdb853707ec8ef6cf367520406a566d0c
Perumal A <112106273+Perumaltuty@users.noreply.github.com>
2023-02-20 11:05:42 +0530

    Create clock_divider_tb.v

---
+++ clock_divider/clock_divider_tb.v
@@ -0,0 +1,21 @@
+module clock_divider_tb;
+reg clk;
+reg rstn;
+wire clk_o;
+clock_divider dut (
+  .clk(clk),
+  .rstn(rstn),
+  .clk_o(clk_o)
+);
+// Clock generator
+initial begin
+		clk = 0;
+		rstn = 0;
+		#50 rstn=1;
+	end
+	always
+	#10 clk=~clk;
+	initial 
+	#100 $finish;
+  
+endmodule
